d2b5c5f1e706cbe6c16f691901f955899a4d90c9
Sahilkhan1996 <sahilkhan199655@gmail.com>
2022-11-24 17:38:57 +0530

    Initializing the Binary search tree program

---
+++ src/com/bridgelabz/binary_search_tree/BinaryTreeExam.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.binary_search_tree;
+
+public class BinaryTreeExam {
+	public static void main(String[] args) {
+		System.out.println("Initializing the Binary search tree program");
+	}
+}
